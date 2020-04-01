77eb9dfcdc186c66f5ffc7fcda1edffeaeb7cc28
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-01 18:52:34 +0530

    clean code

--- CIA/GLOBAL.gd
+++ CIA/GLOBAL.gd
@@ -10,8 +10,32 @@ func _ready():
 	set_physics_process(true)
 	set_process(true)
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+	
+func _physics_process(delta):
+	pass
+
+func mov():
+	if Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x-20, -speed)
+	elif Input.is_action_pressed("ui_right"):
+		motion.x = min(motion.x+20, speed)
+		
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = max(motion.y+20, speed)
+		
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = min(motion.x-20, -speed)
+		
+	else:
+		motion.x = 0
+		motion.y = 0
+
+
 
 
 
