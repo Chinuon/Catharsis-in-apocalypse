b49442c22edfd9cdf581924b51e68fd07653ca37
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-01 17:36:33 +0530

    added global variables

--- CIA/GLOBAL.gd
+++ CIA/GLOBAL.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here.
+var score = 0
+var motion = Vector2()
+var timer : float = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
