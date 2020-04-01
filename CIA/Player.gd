caf15fb5129a93e5e6adae6846d14aebd02f23af
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-01 17:55:11 +0530

    added script

--- CIA/Player.gd
+++ CIA/Player.gd
@@ -4,11 +4,20 @@ extends KinematicBody2D
 onready var motion = GLOBAL.motion
 onready var score = GLOBAL.score
 onready var timer = GLOBAL.timer
+var motion1 = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(true)
+	set_process(true)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_left"):
+		motion1.x = max(motion1.x-20, -500)
+		$AnimatedSprite.play("idle")
