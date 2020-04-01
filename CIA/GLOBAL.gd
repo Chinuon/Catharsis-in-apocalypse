b6e3f6364432b8b21156b0b7b184d740c2f423ff
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-01 18:54:40 +0530

    check point

--- CIA/GLOBAL.gd
+++ CIA/GLOBAL.gd
@@ -5,6 +5,7 @@ var score = 0
 var motion = Vector2()
 var timer : float = 0
 var speed = 500
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
@@ -19,21 +20,6 @@ func _process(delta):
 func _physics_process(delta):
 	pass
 
-func mov():
-	if Input.is_action_pressed("ui_left"):
-		motion.x = max(motion.x-20, -speed)
-	elif Input.is_action_pressed("ui_right"):
-		motion.x = min(motion.x+20, speed)
-		
-	elif Input.is_action_pressed("ui_down"):
-		motion.y = max(motion.y+20, speed)
-		
-	elif Input.is_action_pressed("ui_up"):
-		motion.y = min(motion.x-20, -speed)
-		
-	else:
-		motion.x = 0
-		motion.y = 0
 
 
 
--- CIA/Player.gd
+++ CIA/Player.gd
@@ -19,7 +19,23 @@ func _process(delta):
 
 	
 func _physics_process(delta):
-	GLOBAL.mov()
+
+	if Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x-20, -speed)
+		
+	elif Input.is_action_pressed("ui_right"):
+		motion.x = min(motion.x+20, speed)
+		
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = max(motion.y+20, speed)
+		
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = min(motion.x-20, -speed)
+		
+	else:
+		motion.x = 0
+		motion.y = 0
+		
 	move_and_slide(motion)
 	
 
