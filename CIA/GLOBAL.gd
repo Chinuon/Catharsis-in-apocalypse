7038f5a57b47a53a2d89f31e705697cebb7ffd19
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-05 17:18:48 +0530

    added new  code

--- CIA/GLOBAL.gd
+++ CIA/GLOBAL.gd
@@ -3,6 +3,9 @@ extends Node2D
 # Declare member variables here.
 var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
+const MAX_SPEED = 200
+const ACCELERATION = 10
+const FRICTION = 400
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
--- CIA/Player.gd
+++ CIA/Player.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
-
+onready var m_speed = GLOBAL.MAX_SPEED
 onready var velocity = GLOBAL.velocity
-
+const accel = GLOBAL.ACCELERATION
+const fric = GLOBAL.FRICTION
 func _physics_process(delta):
 	var input_vector = GLOBAL.input_vector
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
+	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * accel * delta
+		velocity = velocity.clamped(m_speed * delta) 
 	else:
-		velocity = Vector2.ZERO
+		velocity = GLOBAL.velocity * delta * fric
 		
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta * m_speed)
 	
 
