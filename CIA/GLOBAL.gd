83efeb859a8b729238e0dc94b4e46ad1c36eadf9
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-05 20:18:30 +0530

    final

--- CIA/GLOBAL.gd
+++ CIA/GLOBAL.gd
@@ -6,21 +6,3 @@ var input_vector = Vector2.ZERO
 const MAX_SPEED = 200
 const ACCELERATION = 10
 const FRICTION = 40
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	set_physics_process(true)
-	set_process(true)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
-func _physics_process(delta):
-	pass
-
-
-
-
-
-
-
