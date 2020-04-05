d7ff6fa88a9ecd64cc085b72d440fa2813eb957e
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-05 16:53:35 +0530

    changes code

--- CIA/GLOBAL.gd
+++ CIA/GLOBAL.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
 # Declare member variables here.
-var score = 0
-var motion = Vector2()
-var timer : float = 0
-var speed = 500
-
+var velocity = Vector2.ZERO
+var input_vector = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
--- CIA/Player.gd
+++ CIA/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-var velocity = Vector2.ZERO
+onready var velocity = GLOBAL.velocity
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
+	var input_vector = GLOBAL.input_vector
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
