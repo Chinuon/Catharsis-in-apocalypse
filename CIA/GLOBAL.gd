5124d22d8e0acea751472509661c5e23b6d50c71
CHAITANYA <Chaitanyas0101@gmail.com>
2020-04-05 17:30:14 +0530

    added collision shape

--- CIA/GLOBAL.gd
+++ CIA/GLOBAL.gd
@@ -5,7 +5,7 @@ var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
 const MAX_SPEED = 200
 const ACCELERATION = 10
-const FRICTION = 400
+const FRICTION = 40
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
